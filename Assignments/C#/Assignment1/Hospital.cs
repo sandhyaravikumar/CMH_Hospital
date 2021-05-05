3dab59eb27760661da93586079d202b50a7fd521
sandhyaravikumar <83642041+sandhyaravikumar@users.noreply.github.com>
2021-05-05 11:37:51 +0530

    Assignment1
    Chinmaya Hospital assignment in c#

---
+++ Assignments/C#/Assignment1/Hospital.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Assignment1
+{
+    public interface IHospital
+    {
+        public void addpatients(Patients p);
+        double getLocationpercent(DateTime d1, DateTime d2);
+    }
+
+}
---
+++ Assignments/C#/Assignment1/Patients.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class Patients
+    {
+        private string name;
+        private string location;
+        private int age;
+        private DateTime date;
+
+        public void Patient(string name, string location, int age, DateTime date)
+        {
+            this.name = name;
+            this.location = location;
+            this.age = age;
+            this.date = date;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getLocation()
+        {
+            return location;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public DateTime getDate()
+        {
+            return date;
+        }
+
+
+    }
+
+
+}
